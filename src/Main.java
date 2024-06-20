da21b702bfae2700eeb66d73ab550ebc0a383ab4
DonatoAletta <donatoalettawork@gmail.com>
2024-06-20 10:30:15 +0200

    Optimized the code

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,12 @@ public class Main {
     }
 
     public static String stringComparator(String string1, String string2){
-        String[] arrayString1 = string1.split(" ");
-        String[] arrayString2 = string2.split(" ");
+
         StringBuilder stringBuilder = new StringBuilder().append("Le due stringhe sono: ");
 
-
-
-        if(arrayString1.length != arrayString2.length){
-            return stringBuilder.append("Diverse").toString();
-        }
-
-        int tmp = 0;
-        for(int i = 0; i < arrayString1.length; i++){
-            if(arrayString1[i].equals(arrayString2[i]) ){
-                tmp++;
-            }
-        }
-
-        if(tmp == arrayString1.length){
+        if(string1.equals(string2)){
             return stringBuilder.append("Uguali").toString();
         }
-            return stringBuilder.append("Diverse").toString();
+        return stringBuilder.append("Diverse").toString();
     }
 }
